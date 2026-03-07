35e38c734900a7e765b468ef8e6b4536e9b3055e
Aman Raj <amanraj7559@gmail.com>
2026-03-07 22:46:09 +0530

    Initial commit

---
+++ Eulers.java
@@ -0,0 +1,18 @@
+public class Eulers {
+    //Tradition method
+    // int phi=0;
+    // for(int i=1;i<n;i++){
+    //     if(euclid(i,n)==1){
+    //         phi++;
+    //     }
+    // }
+    // return phi;
+
+    //Eulers method
+    
+
+
+
+
+    
+}
